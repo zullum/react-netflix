ef77c0c004af3e9fb2750a8777f5e2e864f62807
zullum <zullumsan@gmail.com>
2021-08-04 15:38:15 +0200

    Created JWT auth and CRUD api for Users.

---
+++ api/routes/users.js
@@ -0,0 +1,74 @@
+const router = require("express").Router();
+const User = require("../models/User");
+const CryptoJS = require("crypto-js");
+const verify = require("../verifyToken");
+
+// UPDATE
+router.put("/:id", verify, async (req, res) => {
+  if (req.user.id === req.params.id || req.user.isAdmin) {
+    if (req.body.password) {
+      req.body.password = CryptoJS.AES.encrypt(
+        req.body.password,
+        process.env.SECRET_KEY
+      ).toString();
+    }
+
+    try {
+      const updatedUser = await User.findByIdAndUpdate(
+        req.params.id,
+        {
+          $set: req.body,
+        },
+        { new: true }
+      );
+      res.status(200).json(updatedUser);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  } else {
+    res.status(403).json({ status: "You can only update your own account." });
+  }
+});
+
+// DELETE
+router.delete("/:id", verify, async (req, res) => {
+  if (req.user.id === req.params.id || req.user.isAdmin) {
+    try {
+      await User.findByIdAndDelete(req.params.id);
+      res.status(200).json({ status: "User has been deleted" });
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  } else {
+    res.status(403).json({ status: "You can only delete your own account." });
+  }
+});
+
+// GET
+router.get("/find/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const { password, ...info } = user._doc;
+    res.status(200).json(info);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// GET ALL
+router.get("/", verify, async (req, res) => {
+  const query = req.query.new;
+  if (req.user.isAdmin) {
+    try {
+      const users = query ? await User.find().limit(10) : await User.find();
+      res.status(200).json(users);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  } else {
+    res.status(403).json({ status: "You can not allowed to fetch all users." });
+  }
+});
+// GET USER STATS
+
+module.exports = router;
